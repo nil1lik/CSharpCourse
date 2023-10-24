4da549859fdd8a03b081ab7f311f6e21aa68cb69
Nil <144815514+ajansalt@users.noreply.github.com>
2023-10-24 23:42:00 +0300

    Döngüler

---
+++ Loops/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ForLoop();
+            //WhileLoop();
+            //DoWhileLoop();
+            //ForEachLoop();
+
+        }
+
+
+        private static void ForEachLoop()
+        {
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
+            foreach (var student in students) // Veri tabanından bir tabloyu çektiğimizde datayı çekmek için sıklıkla kullanırız.
+            {
+                //student = "Ahmet"; // Foreach'in içinde elemanlar değiştirilmez.
+                Console.WriteLine(student);
+            }
+        }
+
+        private static void DoWhileLoop()
+        {
+            int number = 10;
+            do
+            {
+                Console.WriteLine(number);
+                number--;
+
+            } while (number >= 11);
+        }
+
+        private static void WhileLoop()
+        {
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is{0}", number);
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}
